6b26e3c4214c4409339d6d2ea761a8d9f81dba2d
Harry Marsh <58450659+Harry-Marsh@users.noreply.github.com>
2020-10-20 12:03:04 +0100

    Update Practical_Session_1.java

--- FileNewProject/src/filenewproject/Practical_Session_1.java
+++ FileNewProject/src/filenewproject/Practical_Session_1.java
@@ -14,10 +14,22 @@ public class Practical_Session_1 {
 
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter username");
+    System.out.println("Enter Length 1");
 
-    String userName = myObj.nextLine();  // Read user input
-    System.out.println("Username is: " + userName);  // Output user input
+    String Length1 = myObj.nextLine();  // Read user input
+    
+       System.out.println("Enter Length 2");
+
+    String Length2 = myObj.nextLine();  // Read user input
+    
+        System.out.println("Enter Length 3");
+
+    String Length3 = myObj.nextLine();  // Read user input
+    
+    
+    System.out.println("Lenght 1: " + Length1);  // Output user input
+    System.out.println("Lenght 2: " + Length2);  // Output user input
+    System.out.println("Lenght 3: " + Length3);  // Output user input
   }
-    }
+}
 
