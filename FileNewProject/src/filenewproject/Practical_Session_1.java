f4186e82043a203cc495fccf8816c1e63b87daa3
Harry Marsh <58450659+Harry-Marsh@users.noreply.github.com>
2020-10-20 12:05:06 +0100

    Update Practical_Session_1.java

--- FileNewProject/src/filenewproject/Practical_Session_1.java
+++ FileNewProject/src/filenewproject/Practical_Session_1.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package filenewproject;
+
 import java.util.Scanner;  // Import the Scanner class
 
 /**
@@ -12,24 +13,22 @@ import java.util.Scanner;  // Import the Scanner class
  */
 public class Practical_Session_1 {
 
-  public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter Length 1");
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter Length 1");
 
-    String Length1 = myObj.nextLine();  // Read user input
-    
-       System.out.println("Enter Length 2");
+        String Length1 = myObj.nextLine();  // Read user input
 
-    String Length2 = myObj.nextLine();  // Read user input
-    
+        System.out.println("Enter Length 2");
+
+        String Length2 = myObj.nextLine();  // Read user input
+
         System.out.println("Enter Length 3");
 
-    String Length3 = myObj.nextLine();  // Read user input
-    
-    
-    System.out.println("Lenght 1: " + Length1);  // Output user input
-    System.out.println("Lenght 2: " + Length2);  // Output user input
-    System.out.println("Lenght 3: " + Length3);  // Output user input
-  }
-}
+        String Length3 = myObj.nextLine();  // Read user input
 
+        System.out.println("Lenght 1: " + Length1);  // Output user input
+        System.out.println("Lenght 2: " + Length2);  // Output user input
+        System.out.println("Lenght 3: " + Length3);  // Output user input
+    }
+}
